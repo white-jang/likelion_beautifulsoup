27c38012da839460d9bee0a03e240a4ceb60d065
white-jang <galaxy__12@naver.com>
2020-09-23 20:03:14 +0900

    멜론 차트 긁어오기

--- main/crawling.py
+++ main/crawling.py
@@ -1,5 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-URL = "http://www.google.com"
-result = requests.get(URL)
-print(result.text)
+iamhuman = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+}
+
+URL = "https://www.melon.com/chart/day/index.htm"
+result = requests.get(URL, headers=iamhuman)
+soup = BeautifulSoup(result.text, "html.parser")
+
+songs = soup.find_all("div", {"class": "ellipsis rank01"})
+
+for i in songs:
+    print(i.find("a").text)
